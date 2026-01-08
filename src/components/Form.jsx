65709392764cecf202151a9bda5cfbd4d13ecee1
alexander lopez <iamlexlopez@gmail.com>
2026-01-08 12:43:02 +0800

    fix eslint error

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -13,14 +13,7 @@ import Message from "./Message";
 import Spinner from "./Spinner";
 import { useCities } from "../contexts/CitiesContext";
 import { useNavigate } from "react-router-dom";
-
-export function convertToEmoji(countryCode) {
-  const codePoints = countryCode
-    .toUpperCase()
-    .split("")
-    .map((char) => 127397 + char.charCodeAt());
-  return String.fromCodePoint(...codePoints);
-}
+import { convertToEmoji } from "../utils/convertCodeToEmoji";
 
 const BASE_URL = "https://api.bigdatacloud.net/data/reverse-geocode-client";
 
